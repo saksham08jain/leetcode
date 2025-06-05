4268a49a0cc44dc4f5d3bd67b743cd0baeb46f25
Saksham Jain <92016981+saksham08jain@users.noreply.github.com>
2025-06-05 23:07:33 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.py
@@ -0,0 +1,14 @@
+# Last updated: 5/6/2025, 11:07:33 pm
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for x in nums:
+            if nums[abs(x)-1] < 0:
+                res.append(abs(x))
+            else:
+                nums[abs(x)-1] *= -1
+        return res
