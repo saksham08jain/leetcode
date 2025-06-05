5effd855dd2908d8db8550729ff67190e237dc32
Saksham Jain <92016981+saksham08jain@users.noreply.github.com>
2025-06-05 23:07:37 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,14 @@
+# Last updated: 5/6/2025, 11:07:37 pm
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        li=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                li.append('FizzBuzz')
+            elif i%3==0:
+                li.append('Fizz')
+            elif i%5==0:
+                li.append('Buzz')
+            else:
+                li.append(str(i))
+        return li
