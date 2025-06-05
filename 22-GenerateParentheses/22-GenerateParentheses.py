a82cf15f83cb18da9d1f34fc99feb14201176679
Saksham Jain <92016981+saksham08jain@users.noreply.github.com>
2025-06-05 23:10:33 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,13 @@
+# Last updated: 5/6/2025, 11:10:33 pm
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dp = [[] for i in range(n + 1)]
+        dp[0].append('')
+        for i in range(n + 1):
+            for j in range(i):
+                dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i - j - 1]]
+        return dp[n]
