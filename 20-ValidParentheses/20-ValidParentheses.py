f0aafa4ea8ee4123e3f072b97380c4af388bd41e
Saksham Jain <92016981+saksham08jain@users.noreply.github.com>
2025-06-05 23:10:36 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 5/6/2025, 11:10:36 pm
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
