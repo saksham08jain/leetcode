af4c3870a9d4db91036664d99e1d6e8dbf6f5afb
Saksham Jain <92016981+saksham08jain@users.noreply.github.com>
2025-06-05 23:10:41 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,27 @@
+# Last updated: 5/6/2025, 11:10:41 pm
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        
+        # Perform replacements individually
+        s = s.replace("CM", "DCCCC")
+        s = s.replace("IV", "IIII")
+        s = s.replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX")
+        s = s.replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC")
+        
+        
+        for char in s:
+            number += translations[char]
+            
+        return number
